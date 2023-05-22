a17be8f40e3f9f504fe4a10322884155f04910e1
pabloN <22187793+pablons2@users.noreply.github.com>
2023-05-22 13:57:36 -0300

    add-new-funcionalidades
    add-new-funcionalidades

--- js_menu.js
+++ js_menu.js
@@ -7,7 +7,7 @@
       const spanNavList = document.querySelectorAll('.span-nav');
       const itemNavMain = document.querySelector('.item-nav-main');
       const flexMenuItem = document.querySelectorAll('.flex-nav-item');
-    
+      const bodypd = document.getElementById('body-pd')
 
       const nav = document.querySelector('nav');
        //pega todos os icones...
@@ -29,7 +29,7 @@
               changeIconToogle.classList.remove('bi-x');
               changeIconToogle.classList.add('bi-list');
               toogleNavMenu.classList.remove('active-toggle');
-              nav.style.width = '4rem';
+              nav.style.width = '4.2rem';
               //recebe o resultado do loop...
               linkNavList.forEach(function (linkNav) {
                 linkNav.style.display = 'none';
@@ -39,6 +39,7 @@
               changeIconToogle.classList.add('bi-x');
               toogleNavMenu.classList.add('active-toggle');
               nav.style.width = '14rem';
+              
               // espera a transição de abertura para mostrar os links <
               function showLinksMenu() {
                 linkNavList.forEach(function (linkNav) {
@@ -47,10 +48,9 @@
               }
               setTimeout(showLinksMenu, 350);
             }
-
+            bodypd.classList.toggle('section-pd')
           }
 
-         
          
         });
       }
@@ -70,4 +70,18 @@
             dropdown.style.display = 'none';
           }
         });
+      });
+
+
+      /*ABRIR MENU COM BTN RESPONSIVO*/
+
+      const responsiveBtn = document.querySelector('#header-mobile');
+
+      responsiveBtn.addEventListener('click', ()=>{
+        if(nav.style.display === 'none'){
+          nav.style.display = 'block';
+        }
+        else{
+          nav.style.display = 'none';
+        }
       });
